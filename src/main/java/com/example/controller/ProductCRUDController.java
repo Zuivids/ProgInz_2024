2416be19a4107a44796340006cf76e48d732347f
Zuivids <martins.zaimis@gmail.com>
2024-04-26 11:40:00 +0300

    Added functions to CRUD controller

--- src/main/java/com/example/controller/ProductCRUDController.java
+++ src/main/java/com/example/controller/ProductCRUDController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.service.IProductCRUDService;
 
@@ -14,10 +15,10 @@ public class ProductCRUDController {
 
 	@Autowired
 	private IProductCRUDService crudService;
-	
+
 	@GetMapping("/all")
 	public String getProductCRUDAll(Model model) {
-		
+
 		try {
 			model.addAttribute("mydata", crudService.retriveAll());
 			return "product-show-many-page";
@@ -25,7 +26,21 @@ public class ProductCRUDController {
 			model.addAttribute("mydata", e.getMessage());
 			return "error-page";
 		}
+
+	}
+
+	@GetMapping("/product")
+	public String getProductId(@RequestParam("id") int id, Model model) {
+		
+		try {
+			model.addAttribute("mydata", crudService.retriveById(id));
+			return "product-show-one-page";
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+
 		
 	}
-	
+
 }
