623867ad16372b37993275b3265b986c1478ab05
Zuivids <martins.zaimis@gmail.com>
2024-05-10 10:49:44 +0300

    Added CRUD functionality

--- src/main/java/com/example/controller/ProductCRUDController.java
+++ src/main/java/com/example/controller/ProductCRUDController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.model.Product;
 import com.example.service.IProductCRUDService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/product/crud")
@@ -34,7 +39,7 @@ public class ProductCRUDController {
 
 	@GetMapping("/product")
 	public String getProductId(@RequestParam("id") int id, Model model) {
-		
+
 		try {
 			model.addAttribute("mydata", crudService.retriveById(id));
 			return "product-show-one-page";
@@ -43,12 +48,11 @@ public class ProductCRUDController {
 			return "error-page";
 		}
 
-		
 	}
-	
+
 	@GetMapping("/product/{id}")
 	public String getProductId2(@PathVariable("id") int id, Model model) {
-		
+
 		try {
 			model.addAttribute("mydata", crudService.retriveById(id));
 			return "product-show-one-page";
@@ -57,23 +61,70 @@ public class ProductCRUDController {
 			return "error-page";
 		}
 
-		
 	}
-	
+
 	@GetMapping("/insert")
 	public String getProductCRUDInsert(Model model) {
-		
+
 		model.addAttribute("product", new Product());
-		
-		return "product-insert-page";
-	}
-	
-	@PostMapping("/insert")
-	public String postProductCRUDInsert(Product product) {
-		
-		
-		System.out.print(product);
+
 		return "product-insert-page";
 	}
 
+	@PostMapping("/insert")
+	public String postProductCRUDInsert(@Valid Product product, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "product-insert-page";
+		} else {
+			crudService.create(product);
+			System.out.print(product);
+			return "product-insert-page";
+		}
+
+	}
+
+	@GetMapping("/update/{id}") // localhost:8080/product/crud/update/1
+	public String getProductCRUDUpdateById(@PathVariable("id") int id, Model model) {
+
+		try {
+			Product productForUpdating = crudService.retriveById(id);
+			model.addAttribute("product", productForUpdating);
+			model.addAttribute("id");
+			return "product-update-page";
+
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+
+	}
+
+	@PostMapping("/update/{id}")
+	public String postPorductCRUDUpdateById(@PathVariable("id") int id, @Valid Product product, BindingResult result,
+			Model model) {
+
+		try {
+			crudService.updateById(id, product);
+			return "redirect:/product/crud/all";
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String getProductCRUDDeleteById(@PathVariable("id") int id, Model model) {
+		
+		try {
+			crudService.deleteById(id);
+			ArrayList<Product>allProducts = crudService.retriveAll();
+			model.addAttribute("mydata",allProducts);
+			return "product-show-many-page";
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+	}
 }
